31a7d9b8e4838142bf7544bd0c6d6832d051309e
Syed Samarul Huda <syed.samarul.huda@flyrobe.com>
2017-01-12 18:45:26 +0530

    First commit

---
+++ RahulJain/TableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  TableViewController.swift
+//  RahulJain
+//
+//  Created by Samarul Huda on 09/01/17.
+//  Copyright © 2017 Samarul Huda. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("rahulCell")
+        
+        return cell!
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
